cbbde8ba3320caa174f9680aac4962705358d693
Raymond Selvik <raymond.selvik@itera.com>
2023-10-16 16:22:38 +0200

    wip

--- src/config/store.py
+++ src/config/store.py
@@ -11,10 +11,26 @@ def init_app_config():
         config['spark_versions']: Dict[str, SparkVersion] = dict()
     
 def add_spark_version(spark_version: SparkVersion):
-    with shelve.open(APP_PATH.joinpath("config")) as config:
-        config['spark_versions'][spark_version.name] = spark_version
+    with shelve.open(APP_PATH.joinpath("config"), writeback=True) as config:
+        spark_versions =  config['spark_versions']
+        spark_versions[spark_version.name] = spark_version
 
 def get_spark_version(spark_version_name: str) -> SparkVersion:
     with shelve.open(APP_PATH.joinpath("config")) as config:
-        return config['spark_versions'][spark_version_name]
+        spark_versions = config['spark_versions']
+        if spark_versions.get(spark_version_name) is None:
+            raise Exception("Spark version does not exist in config.")
+        else:
+            return spark_versions[spark_version_name]
+
+def get_all_spark_versions() -> list[SparkVersion]:
+    with shelve.open(APP_PATH.joinpath("config")) as config:
+        spark_versions = config['spark_versions']
+        return list(spark_versions.values())
+    
+
+def remove_spark_version(spark_version: str):
+    with shelve.open(APP_PATH.joinpath("config"), writeback=True) as config:
+        del config['spark_versions'][spark_version]
+
 
--- tests/app_config/test_store.py
+++ tests/app_config/test_store.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from pathlib import Path
 import shutil
 
-from src.config.store import init_app_config, get_spark_version, add_spark_version
+from src.config import store
 from src.config.models import SparkVersion
 
 
@@ -15,7 +15,7 @@ class AppConfigStoreTestCase(unittest.TestCase):
     @patch("src.config.store.APP_PATH", Path('./tmp'))
     def setUp(self) -> None:
         Path.mkdir(self.TMP_FOLDER, exist_ok=True)
-        init_app_config()
+        store.init_app_config()
 
     
     def tearDown(self) -> None:
@@ -26,11 +26,37 @@ class AppConfigStoreTestCase(unittest.TestCase):
     @patch("src.config.store.APP_PATH", Path('./tmp'))
     def test_add_and_get_spark_version(self):
         expected_spark_version = SparkVersion(name =  "aa", install_path = "bb")
-
-        add_spark_version(expected_spark_version)
-        actual_spark_version = get_spark_version(expected_spark_version.name)
+        
+        store.add_spark_version(expected_spark_version)
+        actual_spark_version = store.get_spark_version(expected_spark_version.name)
 
         assert expected_spark_version == actual_spark_version
+
+    @patch("src.config.store.APP_PATH", Path('./tmp'))
+    def test_add_and_get_all_spark_versions(self):
+        expected_spark_versions = [
+            SparkVersion(name =  "a1", install_path = "b1"),
+            SparkVersion(name =  "a2", install_path = "b2")
+        ]
+
+        store.add_spark_version(expected_spark_versions[0])
+        store.add_spark_version(expected_spark_versions[1])
+
+        actual_spark_versions = store.get_all_spark_versions()
+
+        assert expected_spark_versions == actual_spark_versions
+
+    
+    @patch("src.config.store.APP_PATH", Path('./tmp'))
+    def test_remove_spark_version(self):
+        spark_version  = SparkVersion(name =  "a1", install_path = "b1")
+    
+        store.add_spark_version(spark_version)
+        store.remove_spark_version(spark_version.name)
+
+        self.assertRaises(Exception,  lambda: store.get_spark_version(spark_version.name))
+
+
 
 
 
